7801059fabd07eee9b49e82136fb1b8b64e3bd07
rizkipambudi-jubelio <rizki.pambudi@jubelio.com>
2026-08-02 12:22:13 +0700

    feat: integrate window_manager for desktop window configuration and initialization

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,10 +1,31 @@
+
 import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
 import 'app.dart';
 import 'core/config/flavor_config.dart';
+import 'core/utils/check_platform.dart';
 import 'injection_container.dart' as di;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (CheckPlatform.isWindows || CheckPlatform.isLinux || CheckPlatform.isMacOS) {
+    await windowManager.ensureInitialized();
+
+    WindowOptions windowOptions = const WindowOptions(
+      minimumSize: Size(400, 800),
+      center: true,
+      backgroundColor: Colors.transparent,
+      skipTaskbar: false,
+      titleBarStyle: TitleBarStyle.normal,
+    );
+
+    windowManager.waitUntilReadyToShow(windowOptions, () async {
+      await windowManager.show();
+      await windowManager.focus();
+      await windowManager.maximize();
+    });
+  }
 
   FlavorConfig.initialize(Flavor.development);
 
